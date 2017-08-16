55d74b52edd649b57d2398d8313708126c76f489
tedfoodlin <tedklin@gmail.com>
2017-08-16 11:34:51 -0700

    trying a sharper turn

--- src/Main.java
+++ src/Main.java
@@ -14,13 +14,13 @@ public class Main {
     public static double y0 = 0;
 
     public static double x1 = 0;
-    public static double y1 = 40000;
+    public static double y1 = 4;
 
     public static double x2 = 0;
-    public static double y2 = 115000;
+    public static double y2 = 4;
 
-    public static double x3 = 75000;
-    public static double y3 = 115000;
+    public static double x3 = 3;
+    public static double y3 = 4;
 
     public static double m_cx;
     public static double m_bx;
